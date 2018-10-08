26fa6a0d6424b76ddb4b45bed89a640580d79d15
Isabelle Delmas <isabelle@delmas.us>
2018-10-08 15:50:05 -0700

    creating balloons

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour {
+
+    public GameObject meMyselfEye;
+    public GameObject balloonPrefab;
+    public float floatStrength = 20f;
+
+    private GameObject balloon;
+    private MyInputyController inputController;
+
+	// Use this for initialization
+	void Start () {
+        inputController = meMyselfEye.GetComponent<MyInputyController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inputController.RightTriggerDown())
+        {
+            NewBalloon();
+        } else if (inputController.RightTriggerUp())
+        {
+            ReleaseBalloon();
+        }
+	}
+
+    private void NewBalloon()
+    {
+        balloon = Instantiate(balloonPrefab);
+    }
+
+    private void ReleaseBalloon()
+    {
+        balloon.GetComponent<Rigidbody>().AddForce(Vector3.up * floatStrength);
+        balloon = null;
+    }
+
+}
---
+++ Assets/Scripts/MyInputyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyInputyController : MonoBehaviour {
+
+    public SteamVR_TrackedObject rightHand;
+    private SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool RightTriggerDown()
+    {
+        device = SteamVR_Controller.Input((int)rightHand.index);
+        if(device != null && device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            device.TriggerHapticPulse(700);
+            return true;
+        }
+        return false;
+    }
+
+    public bool RightTriggerUp()
+    {
+        device = SteamVR_Controller.Input((int)rightHand.index);
+        if (device != null && device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            return true;
+        }
+        return false;
+    }
+}
